bd31cf3787cf394ddb53c7431bec74153242a4ff
Shambhu2612 <Shambhu261290@gmail.com>
2025-05-31 01:15:58 +0530

    added excption middleware

---
+++ MyWebApiApp/MyWebApiApp.Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace MyWebApiApp.Api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("before middleware call");
+            await _next(context);
+            Console.WriteLine("after middleware call");
+        }
+    }
+}
